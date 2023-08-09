38fa3e3eb359a63207cf640eb4a35c6fdb63c7f2
areeg <127351347+arejib@users.noreply.github.com>
2023-08-08 19:36:26 -0500

    Broken code for popup

--- medlab/src/components/TestDetail.tsx
+++ medlab/src/components/TestDetail.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import LabTestList from './LabTestList';
 import { AltNameData } from "./AltNameList";
+import Popup from 'reactjs-popup';
+
 
 export interface LabTestData {
     id: number,
@@ -18,30 +20,33 @@ interface TestDetailProps {
     altNameData: AltNameData[],
     }
 
+
+
     const TestDetail: React.FC<TestDetailProps> = ({ selectedTest, altNameData}) => {
 
     return (
         <section>
+        {/* Ensure that the button is properly rendered */}
+        <Popup trigger={<button> Click me</button>} position="right center">
             {selectedTest && (
-            <div>
-                <h2>Test Details</h2>
-                <p>Name: {selectedTest.name}</p>
-                <p>Description: {selectedTest.description}</p>
-                <p>Learn More:
-                <a href={selectedTest.info_url} target="_blank" rel="noopener noreferrer">{selectedTest.info_url}</a>
-                </p>
-                <p>Normal Reference: {selectedTest.normal_reference}</p>
-                <p>Unit of Measure: {selectedTest.unit_of_measure}</p>
-                <p>Alternate Name: </p>
-                <ul>
-                    {altNameData.map((altName)=> (
-                        <li key={altName.id}>{altName.name}</li>
-                    ))}
-                </ul>
-            </div>
+                <div>
+                    <h2>Test Details</h2>
+                    <p>Name: {selectedTest.name}</p>
+                    <p>Description: {selectedTest.description}</p>
+                    <p>Learn More: <a href={selectedTest.info_url} target="_blank" rel="noopener noreferrer">{selectedTest.info_url}</a></p>
+                    <p>Normal Reference: {selectedTest.normal_reference}</p>
+                    <p>Unit of Measure: {selectedTest.unit_of_measure}</p>
+                    <p>Alternate Name:</p>
+                    <ul>
+                        {altNameData.map((altName) => (
+                            <li key={altName.id}>{altName.name}</li>
+                        ))}
+                    </ul>
+                </div>
             )}
-        </section>
-        );
-    };
+        </Popup>
+    </section>
+);
+};
 
     export default TestDetail;
